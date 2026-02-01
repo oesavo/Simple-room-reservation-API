16ad55de57af385ea244a54c2538efaabe943ab6
Oskari Savonen <oskari.savonen99@gmail.com>
2026-02-01 13:20:28 +0200

    Selvennys: time_helpers apufunktioiden palauttamia arvoja selvennetty kommenteilla

--- utils/time_helpers.js
+++ utils/time_helpers.js
@@ -14,22 +14,26 @@ function normalizeToMinute(ms) {
 }
 
 // Check interval validity (numbers, end > start)
+// True if interval is valid
 function isValidInterval(startMs, endMs) {
   return Number.isFinite(startMs) && Number.isFinite(endMs) && endMs > startMs;
 }
 
 // Check if start is in the past (compared at minute precision)
+// True if starts in the past
 function isInPast(startMs) {
   const nowRounded = normalizeToMinute(Date.now());
   return startMs < nowRounded;
 }
 
 // Overlap check for half-open intervals [start, end)
+// True if there is overlap
 function intervalsOverlap(aStart, aEnd, bStart, bEnd) {
   return aStart < bEnd && bStart < aEnd;
 }
 
 // Check if reservation is over 12 hours long to prevent reservations that last for days
+// True if reservation is over 12 hours long
 function isTooLong(startMs, endMs) {
   return ((endMs - startMs) > 12 * 3600000 )
 }
